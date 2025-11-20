88afeff3bd764e5635466522e5e1d350c4ea77cc
Akash9874 <akashdeepdash@gmail.com>
2025-11-20 20:57:54 +0530

    follow up dewployment

--- components/LenisProvider.tsx
+++ components/LenisProvider.tsx
@@ -12,7 +12,6 @@ export default function LenisProvider({ children }: LenisProviderProps) {
     const lenis = new Lenis({
       duration: 1.1,
       smoothWheel: true,
-      smoothTouch: false,
     });
 
     let animationFrameId: number;
